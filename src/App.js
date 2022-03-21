c00c3d3f7a5d913b0c2b459c3e1f59a648fa38f1
Weiyu Tang <twyla9876@outlook.com>
2022-03-21 17:13:55 +0000

    added header component and links to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route } from 'react-router-dom';
 import HomepageComponent from './pages/homepage/HomepageComponent';
 import { Routes } from 'react-router-dom';
 import ShopComponent from './pages/shoppage/ShopComponent';
+import HeaderComponent from './components/header/HeaderComponent';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element = {<HomepageComponent />} />
-      <Route path='/shop' element = {<ShopComponent />} />
-    </Routes>
+    <div>
+      <HeaderComponent />
+      <Routes>
+        <Route exact path='/' element = {<HomepageComponent />} />
+        <Route path='/shop' element = {<ShopComponent />} />
+      </Routes>
+    </div>
   );
 }
 
